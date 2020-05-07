310fd5bdfbb2bb0c872641d4bdb399db07024ea4
Utkarsh Krishna <uksrivas15@gmail.com>
2020-05-08 00:30:58 +0530

    Create First_Bad_Version.py

---
+++ LeetCoding_Challenge_May_2020/week1/First_Bad_Version.py
@@ -0,0 +1,25 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        mid=n//2
+        l=1
+        r=n
+        lastBad=mid
+        while l<=r:
+            mid=int((l+r)/2)
+            if isBadVersion(mid):
+                lastBad=mid
+                r=mid-1
+            else:
+                l=mid+1
+        
+        return lastBad
